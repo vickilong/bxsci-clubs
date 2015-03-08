c012bf8ca91d0e54bf34e2be3a08e0ac92cbc0c6
Vicki Long <longv@bxscience.edu>
2015-03-08 08:55:37 -0400

    Implemented some attendance logging

--- app/src/main/java/clubs/vicki/long0/clubs/AttendanceActivity.java
+++ app/src/main/java/clubs/vicki/long0/clubs/AttendanceActivity.java
@@ -1,23 +1,59 @@
 package clubs.vicki.long0.clubs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by User on 3/8/2015.
  */
 public class AttendanceActivity extends Activity {
 
+    private EditText inputAttendance;
+    private Button logAttendance;
     private Intent i;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+
+        getInit();
+
+        logAttendance.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String userCode = inputAttendance.getText().toString();
+
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Meeting");
+                query.whereEqualTo("Attendance_Code", userCode);
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+                    public void done(ParseObject object, ParseException e) {
+                        if (object == null) {
+                            Log.d("score", "The getFirst request failed.");
+                        } else {
+                            attendanceSuccess();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -52,6 +88,20 @@ public class AttendanceActivity extends Activity {
         i = new Intent(AttendanceActivity.this, LoginActivity.class);
         startActivity(i);
         finish();
+    }
+
+    public void getInit () {
+        inputAttendance = (EditText) findViewById(R.id.attendance_input);
+        logAttendance = (Button) findViewById(R.id.attendance_button);
+    }
+
+    public void attendanceSuccess () {
+        Context context = getApplicationContext();
+        CharSequence text = getResources().getString(R.string.attendance_success);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
 }
