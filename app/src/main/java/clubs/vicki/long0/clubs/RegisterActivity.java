0e1e738352c15959f569f4669659e7bbae320891
Vicki Long <longv@bxscience.edu>
2015-03-08 01:44:36 -0500

    Added user input to registration

--- app/src/main/java/clubs/vicki/long0/clubs/RegisterActivity.java
+++ app/src/main/java/clubs/vicki/long0/clubs/RegisterActivity.java
@@ -1,13 +1,15 @@
 package clubs.vicki.long0.clubs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
@@ -16,6 +18,7 @@ import com.parse.SignUpCallback;
  */
 public class RegisterActivity extends Activity {
 
+    private EditText name, password, email, OSIS, homeroom;
     private Button register, login;
     private Intent i;
 
@@ -23,24 +26,24 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        register = (Button) findViewById(R.id.register_button);
-        login = (Button) findViewById(R.id.register_to_login_button);
+        getInit();
 
         register.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ParseUser user = new ParseUser();
-                user.setUsername("my name");
-                user.setPassword("my pass");
-                user.setEmail("email@example.com");
+                user.setUsername( email.getText().toString() );
+                user.setPassword( password.getText().toString() );
+                user.setEmail( email.getText().toString() );
 
                 // other fields can be set just like with ParseObject
-                user.put("OSIS", 123456789);
-                user.put("Homeroom", "A01");
+                user.put("Name", name.getText().toString() );
+                user.put("OSIS", Integer.parseInt(OSIS.getText().toString()) );
+                user.put("Homeroom", homeroom.getText().toString());
 
                 user.signUpInBackground(new SignUpCallback() {
                     public void done(ParseException e) {
                         if (e == null) {
-                            System.out.println("ASFKEWF");
+                            registerSuccess();
                             goToLogin();
                         } else {
                             // Sign up didn't succeed. Look at the ParseException
@@ -56,6 +59,26 @@ public class RegisterActivity extends Activity {
                 goToLogin();
             }
         });
+    }
+
+    public void getInit () {
+        name = (EditText) findViewById(R.id.register_name);
+        password = (EditText) findViewById(R.id.register_password);
+        email = (EditText) findViewById(R.id.register_email);
+        OSIS = (EditText) findViewById(R.id.register_OSIS);
+        homeroom = (EditText) findViewById(R.id.register_homeroom);
+
+        register = (Button) findViewById(R.id.register_button);
+        login = (Button) findViewById(R.id.register_to_login_button);
+    }
+
+    public void registerSuccess () {
+        Context context = getApplicationContext();
+        CharSequence text = getResources().getString(R.string.register_success);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void goToLogin () {
