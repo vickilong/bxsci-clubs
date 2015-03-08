38fe97bcfdb9e195d6893b0c4c2fa77f39043200
Vicki Long <longv@bxscience.edu>
2015-03-08 06:21:36 -0400

    Styled home page

--- app/src/main/java/clubs/vicki/long0/clubs/AttendanceActivity.java
+++ app/src/main/java/clubs/vicki/long0/clubs/AttendanceActivity.java
@@ -1,13 +1,19 @@
 package clubs.vicki.long0.clubs;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 /**
  * Created by User on 3/8/2015.
  */
-public class AttendanceActivity extends Activity {
+public class AttendanceActivity extends ActionBarActivity {
+
+    private Intent i;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,33 @@ public class AttendanceActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_clubs, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.attendance:
+                return true;
+            case R.id.your_clubs:
+                return true;
+            case R.id.new_club:
+                return true;
+            case R.id.settings:
+                return true;
+            case R.id.logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout () {
+        ParseUser.logOut();
+        i = new Intent(AttendanceActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 
 }
--- app/src/main/java/clubs/vicki/long0/clubs/HomeActivity.java
+++ app/src/main/java/clubs/vicki/long0/clubs/HomeActivity.java
@@ -3,13 +3,10 @@ package clubs.vicki.long0.clubs;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 /**
  * Created by User on 3/8/2015.
@@ -46,29 +43,6 @@ public class HomeActivity extends Activity {
                 finish();
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_clubs, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
